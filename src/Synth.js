4f2a03f543f611302bde3c8d52af10cbb799ed6d
Alex Wilmer <djazium@gmail.com>
2016-10-19 01:02:18 -0400

    :link: github link

--- src/Synth.js
+++ src/Synth.js
@@ -8,10 +8,18 @@ export default () => {
 
   let ctx = new AudioContext()
   let o = ctx.createOscillator()
+  let mod = ctx.createOscillator()
+  let modGain = ctx.createGain()
+
+  mod.type = 'sine'
+  mod.connect(modGain)
+  modGain.connect(o.frequency)
+
   o.type = 'square'
   o.frequency.value = 200
   o.connect(ctx.destination)
   o.start()
+  mod.start()
   o.connect(createScope(ctx))
 
   return state => {
@@ -21,5 +29,7 @@ export default () => {
      */
 
     o.frequency.value = state.knobs.knob1
+    modGain.gain.value = state.knobs.knob2
+    mod.frequency.value = state.knobs.knob3
   }
 }
